ef66aa61a7a7a475c0ee2fae55a5ba7d98c4f025
Zeyu Zhou <zeyu.zhou@tum.de>
2021-05-31 21:55:28 +0200

    minorFix

--- src/components/CreateOrderView/CompanionBox.js
+++ src/components/CreateOrderView/CompanionBox.js
@@ -4,7 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
 import { Divider, ButtonBase, Typography} from "@material-ui/core";
 import logo from "../../images/logo.png";
-import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import Ecoin from "../ECoin";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -64,7 +64,7 @@ function CompanionBox(props) {
                     <Grid container spacing={2} direction="column"> 
                     <Grid item>Details</Grid>
                    
-                <Grid item container flex="row">Price: {props.price}  <span>&nbsp;</span><MonetizationOnIcon color= "secondary" ></MonetizationOnIcon> <span>&nbsp;</span>/ Game </Grid>
+                <Grid item container flex="row">Price: {props.price}  <span>&nbsp;</span><Ecoin></Ecoin> <span>&nbsp;</span>/ Game </Grid>
                 <Grid item>Server: {props.server}</Grid>
                 <Grid item>Platform: {props.platform}</Grid>
                 </Grid></div>

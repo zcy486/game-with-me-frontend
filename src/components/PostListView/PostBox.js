d2e04fca23c0b44d0f7106a1a4fc3a82ba78ee25
Yutong Zhou <ge34goq@mytum.de>
2021-06-01 16:37:26 +0200

    refined post box

--- src/components/PostListView/PostBox.js
+++ src/components/PostListView/PostBox.js
@@ -6,12 +6,16 @@ import Typography from "@material-ui/core/Typography";
 import ButtonBase from "@material-ui/core/ButtonBase";
 import logo from "../../images/logo.png";
 import ECoin from "../ECoin";
+import Rating from "@material-ui/lab/Rating";
 
 const useStyles = makeStyles((theme) => ({
   paper: {
     fontFamily: "Helvetica",
     padding: theme.spacing(2),
     marginBottom: theme.spacing(1),
+  },
+  paperContent: {
+    display: "flex",
   },
   image: {
     width: 128,
@@ -23,6 +27,22 @@ const useStyles = makeStyles((theme) => ({
     maxWidth: "100%",
     maxHeight: "100%",
   },
+  paperColumn: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+    textAlign: "left",
+  },
+  placeHolder: {
+    flexGrow: 1,
+  },
+  priceInfo: {
+    display: "flex",
+  },
+  ratingStars: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
 }));
 
 function PostBox(props) {
@@ -30,34 +50,33 @@ function PostBox(props) {
 
   return (
     <Paper className={classes.paper}>
-      <Grid container spacing={2}>
+      <div className={classes.paperContent}>
         <Grid item>
           <ButtonBase className={classes.image}>
-            <img
-              className={classes.img}
-              alt="avatar"
-              src={logo}
-            />
+            <img className={classes.img} alt="avatar" src={logo} />
           </ButtonBase>
         </Grid>
-        <Grid item xs={12} sm container>
-          <Grid item xs container direction="column" spacing={2}>
-            <Grid item xs>
-              <Typography>
-                {props.username}
-              </Typography>
-            </Grid>
-            <Grid item>
-              5
-            </Grid>
-            
-            <Grid item>
-              <ECoin></ECoin>
-                / Game
-            </Grid>
-          </Grid>
-        </Grid>
-      </Grid>
+        <div className={classes.paperColumn}>
+          <Typography>{props.username}</Typography>
+          <div className={classes.priceInfo}>
+            5<span>&nbsp;</span>
+            <ECoin></ECoin>
+            <span>&nbsp;</span>/ Game
+          </div>
+        </div>
+        <div className={classes.placeHolder} />
+        <div className={classes.paperColumn}>
+          <div className={classes.ratingStars}>
+            <Rating
+              name="half-rating-read"
+              defaultValue={2.5}
+              precision={0.5}
+              readOnly
+            />
+          </div>
+          <Grid item>Language: Español, English</Grid>
+        </div>
+      </div>
     </Paper>
   );
 }

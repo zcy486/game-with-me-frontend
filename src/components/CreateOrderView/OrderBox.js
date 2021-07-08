f8c6c49fde36eb5b0aa617ce626622f6bed252e7
Zeyu Zhou <zeyu.zhou@tum.de>
2021-07-08 18:19:00 +0200

    emphasize amount & correct input in orderBox

--- src/components/CreateOrderView/OrderBox.js
+++ src/components/CreateOrderView/OrderBox.js
@@ -11,167 +11,196 @@ import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
 import DialogTitle from "@material-ui/core/DialogTitle";
 import Ecoin from "../ECoin";
+import { Typography } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    fontFamily: "Helvetica",
-    padding: theme.spacing(5),
-    marginBottom: theme.spacing(7),
-    fontSize: 20,
-    minWidth: 500,
-  },
-  Buttons: {
-    marginLeft: theme.spacing(20),
-  },
-  Button: {
-    marginLeft: theme.spacing(2),
-  },
+    paper: {
+        fontFamily: "Helvetica",
+        padding: theme.spacing(5),
+        marginBottom: theme.spacing(7),
+        fontSize: 20,
+        minWidth: 500,
+    },
+    Buttons: {
+        marginLeft: theme.spacing(20),
+    },
+    
+    Button: {
+        marginLeft: theme.spacing(2),
+    },
+
+    infoRow: {
+        display: "flex",
+    },
 }));
 
 function OrderBox(props) {
-  const classes = useStyles();
+    const classes = useStyles();
 
-  //user selected quantities
-  const [value, setValue] = useState(1);
+    //user selected quantities
+    const [value, setValue] = useState(1);
 
-  const handleChange = (e) => {
-    setValue(e.target.value);
-  };
+    const handleChange = (e) => {
 
-  const onConfirm = (event) => {
-    event.preventDefault();
-    props.onConfirm(totalAmount, props.user._id, props.post._id, props.post.companionId);
-  };
+        const valid = e.target.value.replace(/[e\+\-\.]/, '');
 
-  const onRecharge = (event) => {
-    event.preventDefault();
-    props.onRecharge();
-  };
+        setValue(valid);
+    };
 
-  const [open, setOpen] = React.useState(false);
+    const onConfirm = (event) => {
+        event.preventDefault();
+        props.onConfirm(totalAmount, props.user._id, props.post._id, props.post.companionId);
+    };
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+    const onRecharge = (event) => {
+        event.preventDefault();
+        props.onRecharge();
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const [open, setOpen] = React.useState(false);
 
-  var totalAmount = value * props.price;
-  var insufficient = totalAmount > props.user.balance;
-  var wrongAmount = totalAmount <= 0;
-  return (
-    <Paper className={classes.paper}>
-      <Grid container spacing={2}>
-        <Grid item> Game: {props.gameName}</Grid>
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
 
-        <Grid item container flex="row">
-          Price: {props.price} <span>&nbsp;</span>
-          <Ecoin/>
-          <span>&nbsp;</span>/ Game
-        </Grid>
+    const handleClose = () => {
+        setOpen(false);
+    };
 
-        <Grid item container alignItems="center" flex="row">
-          Amount:
-          <span>&nbsp;</span>
-          <TextField
-            id="outlined-number"
-            type="number"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            error={value < 1}
-            variant="outlined"
-            defaultValue={1}
-            required={true}
-            onChange={handleChange}
-            InputProps={{ inputProps: { min: 1 } }}
-          />
-        </Grid>
-        <Grid item container flex="row">
-          Total: {totalAmount}
-          <span>&nbsp;</span>
-          <Ecoin/>
-          <span>&nbsp;</span>
-        </Grid>
-        <Grid item container flex="row">
-          My wallet: {props.user.balance}
-          <span>&nbsp;</span>
-          <Ecoin/>
-          <span>&nbsp;</span>
-        </Grid>
+    var totalAmount = value * props.price;
+    var insufficient = totalAmount > props.user.balance;
+    var wrongAmount = totalAmount <= 0;
+    return (
+        <Paper className={classes.paper}>
+            <Grid container spacing={2}>
+                <Grid item> Game: {props.gameName}</Grid>
 
-        <Grid item>
-          <div className={classes.Buttons}>
-            {insufficient && !wrongAmount ? (
-              <Button
-                className={classes.Button}
-                variant="contained"
-                onClick={onRecharge}
-                color="secondary"
-                type="submit"
-              >
-                Recharge
-              </Button>
-            ) : null}
-            <Tooltip
-              title={
-                insufficient
-                  ? "You don't have enough ecoins, please recharge."
-                  : wrongAmount
-                  ? "Please choose a valid amount bigger than 1."
-                  : ""
-              }
-            >
-              <span>
-                <Button
-                  className={classes.Button}
-                  variant="contained"
-                  color="secondary"
-                  onClick={handleClickOpen}
-                  disabled={insufficient || wrongAmount}
-                  type="submit"
-                >
-                  Confirm & Pay
-                </Button>
-                <Dialog
-                  open={open}
-                  onClose={handleClose}
-                  aria-labelledby="alert-dialog-title"
-                  aria-describedby="alert-dialog-description"
-                >
-                  <DialogTitle id="alert-dialog-title">
-                    {"Please confirm your order"}
-                  </DialogTitle>
-                  <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                      For this order you have to pay {totalAmount} ecoins to the
-                      gaming companion.
-                    </DialogContentText>
-                    <DialogContentText id="alert-dialog-description">
-                      Your wallet balance after this order will be{" "}
-                      {props.user.balance - totalAmount} ecoins.
-                    </DialogContentText>
-                  </DialogContent>
-                  <DialogActions>
-                    <Button onClick={onConfirm} color="secondary" autoFocus>
-                      Confirm & Pay
-                    </Button>
-                    <Button onClick={handleClose}>Cancel</Button>
-                  </DialogActions>
-                </Dialog>
-              </span>
-            </Tooltip>
-            <span>&nbsp;</span>
-            <Button className={classes.Button} onClick={props.onCancel}>
-              Cancel
-            </Button>
-          </div>
-        </Grid>
-      </Grid>
-    </Paper>
-  );
+                <Grid item container flex="row">
+                    Price: {props.price} <span>&nbsp;</span>
+                    <Ecoin />
+                    <span>&nbsp;</span>/ Game
+                </Grid>
+
+                <Grid item container alignItems="center" flex="row">
+                    Amount:
+                    <span>&nbsp;</span>
+                    <TextField
+                        id="outlined-number"
+                        type="number"
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        error={value < 1}
+                        variant="outlined"
+                        defaultValue={1}
+                        required={true}
+                        onChange={handleChange}
+                        value={value}
+                        InputProps={{ inputProps: { min: 1 } }}
+                    />
+                </Grid>
+                <Grid item container flex="row">
+                    Total: {totalAmount}
+                    <span>&nbsp;</span>
+                    <Ecoin />
+                    <span>&nbsp;</span>
+                </Grid>
+                <Grid item container flex="row">
+                    My wallet: {props.user.balance}
+                    <span>&nbsp;</span>
+                    <Ecoin />
+                    <span>&nbsp;</span>
+                </Grid>
+
+                <Grid item>
+                    <div className={classes.Buttons}>
+                        {insufficient && !wrongAmount ? (
+                            <Button
+                                className={classes.Button}
+                                variant="contained"
+                                onClick={onRecharge}
+                                color="secondary"
+                                type="submit"
+                            >
+                                Recharge
+                            </Button>
+                        ) : null}
+                        <Tooltip
+                            title={
+                                insufficient
+                                    ? "You don't have enough ecoins, please recharge."
+                                    : wrongAmount
+                                        ? "Please choose a valid amount bigger than 1."
+                                        : ""
+                            }
+                        >
+                            <span>
+                                <Button
+                                    className={classes.Button}
+                                    variant="contained"
+                                    color="secondary"
+                                    onClick={handleClickOpen}
+                                    disabled={insufficient || wrongAmount}
+                                    type="submit"
+                                >
+                                    Confirm & Pay
+                                </Button>
+                                <Dialog
+                                    open={open}
+                                    onClose={handleClose}
+                                    aria-labelledby="alert-dialog-title"
+                                    aria-describedby="alert-dialog-description"
+                                >
+                                    <DialogTitle id="alert-dialog-title">
+                                        {"Please confirm your order"}
+                                    </DialogTitle>
+                                    <DialogContent>
+                                        <DialogContentText id="alert-dialog-description">
+
+                                            <Typography className={classes.infoRow}>
+                                                For this order you have to pay <span>&nbsp;</span>
+
+                                                <Box fontWeight='fontWeightMedium' display="inline" color="black">
+                                                    {totalAmount}
+
+                                                </Box>
+                                                <Ecoin />
+                                                <span>&nbsp;</span>
+
+                                                to the gaming companion.
+                                            </Typography >
+                                        </DialogContentText>
+                                        <DialogContentText id="alert-dialog-description">
+                                            <Typography className={classes.infoRow}>
+                                                Your wallet balance after this order will be
+                                                <span>&nbsp;</span>
+                                                <Box fontWeight='fontWeightMedium' display="inline" color="black">
+                                                    {props.user.balance - totalAmount}
+                                                </Box>
+                                                <Ecoin /> .
+                                                <span>&nbsp;</span>
+                                            </Typography>
+                                        </DialogContentText>
+                                    </DialogContent>
+                                    <DialogActions>
+                                        <Button onClick={onConfirm} color="secondary" autoFocus>
+                                            Confirm & Pay
+                                        </Button>
+                                        <Button onClick={handleClose} >Cancel</Button>
+                                    </DialogActions>
+                                </Dialog>
+                            </span>
+                        </Tooltip>
+                        <span>&nbsp;</span>
+                        <Button className={classes.Button} onClick={props.onCancel} color="primary"  variant="contained">
+                            Cancel
+                        </Button>
+                    </div>
+                </Grid>
+            </Grid>
+        </Paper>
+    );
 }
 
 export default OrderBox;
